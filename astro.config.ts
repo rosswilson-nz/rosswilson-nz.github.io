1cfd83aa664dcff30a96af09d6ea7f1765b18ddb
Ross Wilson <ross.wilson@otago.ac.nz>
2024-10-21 15:14:18 +1300

    add GitHub Action to deploy to github.io

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
+  site: 'https://rosswilson-nz.github.io',
 
   integrations: [
     tailwind({
